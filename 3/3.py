54b31512c8264dac64796188bf24d7019b3fa0fc
Max Wilson <max.wilson@nottingham.ac.uk>
2021-12-03 11:39:50 +0000

    dodgy solution for day 3 part 2

--- 3/3.py
+++ 3/3.py
@@ -1,24 +1,66 @@
-countof1s = [0]*12
+import time
+
+datasize=12
+dataset = []
+originalcountof1s = 0
+countof1s = 0
 datalength = 0
-gamma = ""
-epsilon = ""
+oxy = ""
+c02 = ""
+
+def myfilter(dataset, position, value):
+    filtereddata = []
+    nextcountof1s = 0
+    if len(dataset) > 1:
+        for row in dataset:
+            if row[position] == value:
+                filtereddata.append(row)
+                if position < datasize-1:
+                    nextcountof1s+=int(row[position+1])
+                else: 
+                    nextcountof1s = -1
+    else:
+        nextcountof1s = -1
+        filtereddata = dataset
+    return filtereddata, nextcountof1s
+
 while True:
     line = input()
     if line == '':
         break
     content = list(line)
-    datalength+=1
-    for x in range(12):
-        countof1s[x]+=int(content[x])
-print(countof1s)
-for x in range(12):
-    if countof1s[x] > datalength/2:
-        gamma = gamma + "1"
-        epsilon = epsilon + "0"
+    dataset.append(content)
+    originalcountof1s += int(content[0])
+
+print(dataset)
+oxydata = dataset
+co2data = dataset
+
+countof1s = originalcountof1s
+for x in range(datasize):
+    print(str(countof1s) + " - " + str(len(oxydata)/2))
+    if countof1s >= len(oxydata)/2:
+        oxydata, countof1s = myfilter(oxydata,x,"1")
     else:
-        gamma = gamma + "0"
-        epsilon = epsilon + "1"
-gammaint = int(gamma,2)
-epsilonint = int(epsilon,2)
-answer = gammaint*epsilonint
+        oxydata, countof1s = myfilter(oxydata,x,"0")
+    print(oxydata)
+    if countof1s == -1:
+        break
+
+countof1s = originalcountof1s
+for x in range(datasize):
+    print(str(countof1s) + " - " + str(len(co2data)/2))
+    if countof1s >= len(co2data)/2:
+        co2data, countof1s = myfilter(co2data,x,"0")
+    else:
+        co2data, countof1s = myfilter(co2data,x,"1")
+    print(co2data)
+    if countof1s == -1:
+        break
+
+oxyint = int(''.join(oxydata[0]),2)
+co2int = int(''.join(co2data[0]),2)
+answer = oxyint*co2int
 print("answer: " + str(answer))
+
+
