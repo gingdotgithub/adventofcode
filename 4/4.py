39141441e31f5460ce2c01aef0eb114780102095
Max Wilson <max.wilson@nottingham.ac.uk>
2021-12-04 11:38:33 +0000

    completed part 2 of day 4

--- 4/4.py
+++ 4/4.py
@@ -13,7 +13,7 @@ def checkboard(boardtocheck):
         mysum = 0
         for row in boardtocheck:
             myvalues = list(row.values())
-            print(myvalues)
+            #print(myvalues)
             mysum+=myvalues[y]
         if mysum == 5:
                 return True
@@ -43,6 +43,7 @@ while True:
 print(boards)
 boardcounts = [0]*len(boards)
 lastcheckcounts = [0]*len(boards)
+countboardsleft = len(boards)
 
 print(boardcounts)
 print(lastcheckcounts)
@@ -55,14 +56,22 @@ for x in inputs:
         for row in board:
             if x in row:
                 row[x] = 1
-                boardcounts[currboard]+=1
+                if boardcounts[currboard] >= 0:
+                    boardcounts[currboard]+=1
     print(boardcounts)
 
-    for y in range(len(boardcounts)):
-        if boardcounts[y] > 5 and boardcounts[y] > lastcheckcounts[y]:
-            lastcheckcounts[y] = boardcounts[y]
-            found = checkboard(boards[y])
-            if found == True:
-                print("answer = " + str(calculateanswer(boards[y])))
+    if countboardsleft == 1:
+        for finalboard in range(len(boardcounts)):
+            if boardcounts[finalboard] > 0:
+                print("answer = " + str(calculateanswer(boards[finalboard])) + "curr item: " + currentinput)
                 sys.exit()
+    else:
+        for y in range(len(boardcounts)):
+            if boardcounts[y] > 5 and boardcounts[y] > lastcheckcounts[y]:
+                lastcheckcounts[y] = boardcounts[y]
+                found = checkboard(boards[y])
+                if found == True and len(boards) > 1:
+                    boardcounts[y] = -1
+                    lastcheckcounts[y] = -1
+                    countboardsleft -= 1
         
