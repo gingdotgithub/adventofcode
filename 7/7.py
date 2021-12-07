fa92583cd5c02d28ba945dcecbf5fa7d3ae0d444
Max Wilson <max.wilson@nottingham.ac.uk>
2021-12-07 07:22:02 +0000

    day 7 sorted. nice.

--- 7/7.py
+++ 7/7.py
@@ -1,11 +1,18 @@
 import statistics
+import math
+
+def calcfuel(n):
+    for i in range(n):
+        n+= i
+    return n
+
 crabsubsstr = input().split(',')
 crabsubs = [int(i) for i in crabsubsstr]
-crabmedian = int(statistics.median(crabsubs))
+crabmean = int(math.floor(statistics.mean(crabsubs)))
 
-print(str(crabmedian))
+print(str(crabmean))
 
 fuel = 0
 for x in crabsubs:
-    fuel+=abs(x-crabmedian)
+    fuel+=calcfuel(abs(x-crabmean))
 print(fuel)
