a30a6f75c247de90e39bcb88f709b501e4991c93
Max Wilson <max.wilson@nottingham.ac.uk>
2021-12-13 09:15:21 +0000

    sorted part 2 day 13 - hard to read!

--- 13/13.py
+++ 13/13.py
@@ -1,22 +1,16 @@
 import itertools
 coords = []
 folds = []
-maxy = 0
-maxx = 0
+lasty = 0
+lastx = 0
 
 def readcoords():
-    global maxy
-    global maxx
     global coords
     while True:
         newline = input()
         if newline == '':
             return False  
         newcoord = list(map(int,newline.split(',')))
-        if newcoord[1] > maxy:
-            maxy = newcoord[1]
-        if newcoord[0] > maxx:
-            maxx = newcoord[0]
         coords.append(newcoord)
 
 def readfolds():
@@ -32,20 +26,29 @@ def readfolds():
 readcoords()
 readfolds()
 
-fold = folds[0]
-for coord in coords:
-    if fold[0] == 'y' and coord[1] > fold[1]:
-        temp = abs(coord[1]-(fold[1]*2))
-        print(str(coord) + " becomes " + str([coord[0],temp]))
-        coord[1] = temp
-    if fold[0] == 'x' and coord[0] > fold[1]:
-        temp = abs(coord[0]-(fold[1]*2))
-        print(str(coord) + " becomes " + str([temp,coord[1]]))
-        coord[0] = temp
-coords.sort()   
-print(coords)  
-coords = list(coords for coords,_ in itertools.groupby(coords))
+for fold in folds:
+    for coord in coords:
+        if fold[0] == 'y' and coord[1] > fold[1]:
+            temp = abs(coord[1]-(fold[1]*2))
+            print(str(coord) + " becomes " + str([coord[0],temp]))
+            coord[1] = temp
+            lasty = fold[1]
+        if fold[0] == 'x' and coord[0] > fold[1]:
+            temp = abs(coord[0]-(fold[1]*2))
+            print(str(coord) + " becomes " + str([temp,coord[1]]))
+            coord[0] = temp
+            lastx = fold[1]
+    coords.sort()   
+    #print(coords)  
+    coords = list(coords for coords,_ in itertools.groupby(coords))
+    
 print(coords)
-print(str(maxy))
-print(str(maxx))
 print(str(len(coords)))
+for y in range(lasty):
+    output = ""
+    for x in range(lastx):
+        if [x,y] in coords:
+            output+="#"
+        else:
+            output+= "."
+    print(output)
