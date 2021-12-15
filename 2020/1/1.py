81cf7de61b8074f58ee9a588f85d0efc19e56e1b
Max Wilson <max.wilson@nottingham.ac.uk>
2021-12-15 21:16:57 +0000

    sorted 2020 day 1

--- 2020/1/1.py
+++ 2020/1/1.py
@@ -5,7 +5,10 @@ while True:
         break
     readvalues.append(newline)
 
-for readvalue in readvalues:
-    if str(2020-int(readvalue)) in readvalues:
-        print(str(int(readvalue)*(2020-int(readvalue))))
-        break
+minvalue = min(readvalues)
+sortedvalues = sorted(readvalues)
+for x in range(len(sortedvalues)):
+    for y in range(x+1,len(sortedvalues)):
+        if str(2020-int(sortedvalues[x])-int(sortedvalues[y])) in sortedvalues:
+            print(str(int(sortedvalues[x])*int(sortedvalues[y])*(2020-int(sortedvalues[x])-int(sortedvalues[y]))))
+            break
