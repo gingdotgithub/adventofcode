68a7682860c8a2155f016ceb7a2000ead319832c
Max Wilson <max.wilson@nottingham.ac.uk>
2021-12-15 09:15:40 +0000

    simple take on dijkstra, since theres no need to actually traverse a graph, just cycle across the grid instead, but building the distances in the dijkstra way

---
+++ 15/15.py
@@ -0,0 +1,30 @@
+from collections import defaultdict
+room = []
+
+while True:
+    newline = input()
+    if newline == '':
+        break
+    roomline = list(map(int,list(newline)))
+    room.append(roomline)
+
+distances = defaultdict(lambda:float("inf"))
+distances["0,0"] = 0
+
+for y in range (len(room)):
+    for x in range(len(room[0])):
+        if (x-1) >= 0:
+            if distances[str(x-1)+","+str(y)] + room[y][x] < distances[str(x)+","+str(y)]:
+                distances[str(x)+","+str(y)] = distances[str(x-1)+","+str(y)] + room[y][x]
+        if (y-1) >= 0:
+            if distances[str(x)+","+str(y-1)] + room[y][x] < distances[str(x)+","+str(y)]:
+                distances[str(x)+","+str(y)] = distances[str(x)+","+str(y-1)] + room[y][x]
+        if (x+1) < len(room[0]):
+            if distances[str(x+1)+","+str(y)] + room[y][x] < distances[str(x)+","+str(y)]:
+                distances[str(x)+","+str(y)] = distances[str(x+1)+","+str(y)] + room[y][x]
+        if (y+1) < len(room):
+            if distances[str(x)+","+str(y+1)] + room[y][x] < distances[str(x)+","+str(y)]:
+                distances[str(x)+","+str(y)] = distances[str(x)+","+str(y+1)] + room[y][x]
+
+for cell in distances.keys():
+    print("distance to " + cell + " = " + str(distances[cell]))
