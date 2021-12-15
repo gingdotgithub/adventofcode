7138774b530d058b03cd138b922907fd28e87460
Max Wilson <max.wilson@nottingham.ac.uk>
2021-12-15 09:19:16 +0000

    actually i didnt need to do half those checks

--- 15/15.py
+++ 15/15.py
@@ -19,12 +19,6 @@ for y in range (len(room)):
         if (y-1) >= 0:
             if distances[str(x)+","+str(y-1)] + room[y][x] < distances[str(x)+","+str(y)]:
                 distances[str(x)+","+str(y)] = distances[str(x)+","+str(y-1)] + room[y][x]
-        if (x+1) < len(room[0]):
-            if distances[str(x+1)+","+str(y)] + room[y][x] < distances[str(x)+","+str(y)]:
-                distances[str(x)+","+str(y)] = distances[str(x+1)+","+str(y)] + room[y][x]
-        if (y+1) < len(room):
-            if distances[str(x)+","+str(y+1)] + room[y][x] < distances[str(x)+","+str(y)]:
-                distances[str(x)+","+str(y)] = distances[str(x)+","+str(y+1)] + room[y][x]
 
 for cell in distances.keys():
     print("distance to " + cell + " = " + str(distances[cell]))
