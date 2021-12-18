4d646ba9a5514e461d5c4b91f8aa5f36559ec2b0
Max Wilson <max.wilson@nottingham.ac.uk>
2021-12-18 20:15:58 +0000

    thank effing goodness

---
+++ 2021/18/18.py
@@ -0,0 +1,124 @@
+import json
+import math
+
+def checkneedsreducing(currentsf):
+    anylist = list(range(10,99))
+    depth = 0
+    literalfound = 0
+    stringversion = str(currentsf)
+    depthindexfound = len(stringversion)
+    literalindexfound = depthindexfound
+    for x in range(len(stringversion)):
+        if stringversion[x] == "[":
+            depth+=1
+        if stringversion[x] == "]":
+            depth-=1
+        if depth == 5:
+            depthindexfound = x
+            return 1, depthindexfound
+    for item in anylist:
+        if str(item) in stringversion:
+            foundindex = stringversion.index(str(item))
+            if foundindex < literalindexfound:
+                literalindexfound = foundindex
+    if literalindexfound < len(stringversion):
+        return 2, literalindexfound
+    return 0, 0
+    #if depthindexfound == len(stringversion) and literalindexfound == len(stringversion):
+    
+    #elif depthindexfound < literalindexfound:
+        
+    #else:
+        #return 2, literalindexfound
+
+def splitliteral(currentsf, indexsf):
+    stringversion = str(currentsf)
+    left = stringversion[0:indexsf]
+    if stringversion[indexsf+2:indexsf+3].isdigit():
+        literalstrlen = 3
+    else:    
+        literalstrlen = 2
+    literalstr = stringversion[indexsf:indexsf+literalstrlen]
+    newstr = "[" + str(math.floor(int(literalstr)/2)) + "," + str(math.ceil(int(literalstr)/2)) + "]"
+    right = stringversion[indexsf+literalstrlen:]
+    return json.loads(left+newstr+right)
+
+def explodepair(currentsf,indexsf):
+    stringversion = str(currentsf)
+    left = stringversion[0:indexsf]
+    nextbracket = stringversion.index("]",indexsf)
+    (pair0,pair1) = stringversion[indexsf+1:nextbracket].split(", ")
+    right = stringversion[nextbracket+1:]
+    x = len(left)-1
+    while x > 0:
+        if (left[x-1:x+1]).isdigit():
+            digit = int(left[x-1:x+1])+int(pair0)
+            left = left[0:x-1] + str(digit) + left[x+1:]
+            break
+        if (left[x:x+1]).isdigit():
+            digit = int(left[x:x+1])+int(pair0)
+            left = left[0:x] + str(digit) + left[x+1:]
+            break
+        x-=1
+    x = 0
+    while x < len(right)-2:
+        # if (right[x:x+3]).isdigit():
+        #     digit = int(right[x:x+3])+int(pair1)
+        #     right = right[0:x] + str(digit) + right[x+3:]
+        #     break
+        if (right[x:x+2]).isdigit():
+            digit = int(right[x:x+2])+int(pair1)
+            right = right[0:x] + str(digit) + right[x+2:]
+            break
+        if (right[x:x+1]).isdigit():
+            digit = int(right[x:x+1])+int(pair1)
+            right = right[0:x] + str(digit) + right[x+1:]
+            break
+        x+=1
+    return json.loads(left+"0"+right)
+
+def calc_magnitude(mylist):
+    total = 0  # don't use `sum` as a variable name
+    part1 = 0
+    part2 = 0
+    if isinstance(mylist[0],int):
+        part1 = mylist[0]
+    else:
+        part1 = calc_magnitude(mylist[0])
+    if isinstance(mylist[1],int):
+        part2 = mylist[1]
+    else:
+        part2 = calc_magnitude(mylist[1])
+    return (3*part1) + (2*part2)
+
+snailfish = []
+while True:
+    newline = input()
+    if newline == '':
+        break
+    snailfish.append(json.loads(newline))
+
+ongoingsum = snailfish.pop(0)
+for sf in snailfish:
+    ongoingsum = [ongoingsum,sf]
+    needsreducing, indexoffound = checkneedsreducing(ongoingsum)
+    while needsreducing > 0:
+        print("needs reducing  - " + str(ongoingsum))
+        if needsreducing == 1:
+            print("doing depth")
+            ongoingsum = explodepair(ongoingsum, indexoffound)
+        elif needsreducing == 2:
+            print("doing literal")
+            ongoingsum = splitliteral(ongoingsum, indexoffound)
+        needsreducing, indexoffound = checkneedsreducing(ongoingsum)
+
+print("final answer: " + str(ongoingsum)) 
+print(calc_magnitude(ongoingsum))
+
+
+# test = json.loads("[[[[1,1],[2,2]],[3,3]],[4,4]]")
+# answer = calc_magnitude(test)
+# answer = json.loads("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]")
+# print(test)
+# print(answer)
+
