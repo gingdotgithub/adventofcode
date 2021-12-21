e279bcf2f09116a6a29c76d6b2551efa5b279c10
Max Wilson <max.wilson@nottingham.ac.uk>
2021-12-21 17:24:33 +0000

    day 2 part 2 sorted for 2020, and updated readmes

--- 2020/2/2.py
+++ 2020/2/2.py
@@ -8,7 +8,15 @@ while True:
     charrange = list(map(int,parts[0].split("-")))
     charcheck = parts[1][0:1]
     charcount = parts[2].count(charcheck)
-    if charcount >= charrange[0] and charcount <= charrange[1]:
-        validpasswords += 1 
+    # if charcount >= charrange[0] and charcount <= charrange[1]:
+    #     validpasswords += 1 
+    validitycheck = 0
+    if charrange[0] <= len(parts[2]) and parts[2][charrange[0]-1:charrange[0]] == charcheck:
+        validitycheck += 1
+    if charrange[1] <= len(parts[2]) and parts[2][charrange[1]-1:charrange[1]] == charcheck:
+        validitycheck += 1
+    if validitycheck == 1:
+        validpasswords+=1 
+
 
 print(str(validpasswords))
---
+++ 2020/README.md
@@ -0,0 +1,7 @@
+# Advent of Code 2020
+
+
+| Day | Part 1           | Part 2           |
+|-----|------------------|------------------|
+| 1   | [:star:](../../../blob/280d40b873a818e0ad355dea4b65bf9b939dbdb2/2020/1/1.py) | [:star:](1/1.py) |
+| 2   | [:star:](../../../blob/b66065cb21f9cf15aa79b91ece2c9c26b662777e/2020/2/2.py) | [:star:](2/2.py) |
