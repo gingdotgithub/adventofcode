7bf7111703fabd936599dc40006f728868a67c25
Max Wilson <max.wilson@nottingham.ac.uk>
2021-12-30 13:35:56 +0000

    finished off day 4

--- 2020/4/4.py
+++ 2020/4/4.py
@@ -4,6 +4,42 @@ passports = []
 optcomponents = ['cid']
 processcount = 0
 
+def validate_data(dtype,inputval):
+    if dtype == 'byr' and inputval.isnumeric():
+        year = int(inputval)
+        if 1920 <= year <= 2002:
+            return True
+    elif dtype == 'iyr' and inputval.isnumeric():
+        year = int(inputval)
+        if 2010 <= year <= 2020:
+            return True
+    elif dtype == 'eyr' and inputval.isnumeric():
+        year = int(inputval)
+        if 2020 <= year <= 2030:
+            return True
+    elif dtype == 'hgt' and ("in" in inputval or "cm" in inputval):
+        measuretype = inputval[-2:]
+        measureval = int(inputval[0:-2])
+        if measuretype == 'in' and 59 <= measureval <= 76:
+            return True
+        elif measuretype == 'cm' and 150 <= measureval <= 193:
+            return True
+    elif dtype =='hcl':
+        if len(inputval) == 7:
+            sixcheck = 0
+            for i in range(len(inputval)):
+                if inputval[i+1:i+2] in ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']:
+                    sixcheck+=1
+            if sixcheck == 6:
+                return True
+    elif dtype == 'ecl':
+        if inputval in ['amb','blu','brn','gry','grn','hzl','oth']:
+            return True
+    elif dtype == 'pid' and inputval.isnumeric() and len(inputval) == 9:
+        return True
+    return False
+
+
 def read_passport(newline):
     global reqcomponents
     global pcount
@@ -18,7 +54,8 @@ def read_passport(newline):
         for x in range(len(parts)):
             part = parts[x].split(":")
             if part[0] in components.keys():
-                components[part[0]]+=1
+                if validate_data(part[0],part[1]):
+                    components[part[0]]+=1
         newline = input()
 
 while True:
