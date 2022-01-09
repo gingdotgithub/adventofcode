d6be62c20c77669d65fd673b86c0575f107de596
Max Wilson <max.wilson@nottingham.ac.uk>
2022-01-09 12:41:17 +0000

    2020 day 8 part 2 - bit of a brute force. probably a way to check/change fewer commands.

--- 2020/8/8.py
+++ 2020/8/8.py
@@ -1,3 +1,5 @@
+import copy
+
 instructions = []
 mainvalue = 0
 
@@ -7,18 +9,40 @@ while True:
         break
     instructions.append([newline,0])
 
-instID = 0
+def process_commands(instructs,target):
+    global mainvalue
+    mainvalue = 0
+    maxinstID = 0
+    instID = 0
+    while True:
+        if instID >= len(instructs):
+            return True
+        if instID > maxinstID:
+            maxinstID = instID
+        inst,count = instructs[instID]
+        if count == target:
+            print(str(maxinstID)+" "+instructs[maxinstID][0])
+            break
+        instructs[instID][1]+=1
+        if inst[0:3] == 'acc':
+            mainvalue += int(inst[4:])
+        if inst[0:3] == 'jmp':
+            instID+= int(inst[4:])
+            continue
+        instID+=1
+    return False
+
+#process_commands(instructions,1)
+x = 0
 while True:
-    inst,count = instructions[instID]
-    if count == 1:
+    alteredinstructions = copy.deepcopy(instructions)
+    if alteredinstructions[x][0][0:3] == 'nop':
+        alteredinstructions[x][0] = 'jmp' + alteredinstructions[x][0][3:]
+    elif alteredinstructions[x][0][0:3] == 'jmp':
+        alteredinstructions[x][0] = 'nop' + alteredinstructions[x][0][3:]
+    if process_commands(alteredinstructions,1):
         break
-    instructions[instID][1]+=1
-    if inst[0:3] == 'acc':
-        mainvalue += int(inst[4:])
-    if inst[0:3] == 'jmp':
-        instID+= int(inst[4:])
-        continue
-    instID+=1
+    x+=1
+
 
 print(mainvalue)
-
