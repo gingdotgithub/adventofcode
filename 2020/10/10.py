efd70ad7334a6f0dd3fbf592a6cb97389e27f0d1
Max Wilson <max.wilson@nottingham.ac.uk>
2022-01-13 07:23:45 +0000

    just tidied part 1 day 10 2020 a little

--- 2020/10/10.py
+++ 2020/10/10.py
@@ -2,19 +2,17 @@ from collections import Counter
 jolters = []
 distances = Counter()
 
+jolters.append(0)
 while True:
     newline = input()
     if newline == '':
         break
     jolters.append(int(newline))
-
 jolters = sorted(jolters)
+jolters.append(jolters[len(jolters)-1]+3)
+#print(jolters)
 
-print(jolters)
-distances[jolters[0]-0]+=1
 for x in range(len(jolters)-1):
     distances[jolters[x+1]-jolters[x]]+=1
-distances[3]+=1
 
-print(distances)
 print(distances[1]*distances[3])
