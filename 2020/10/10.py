529783330578401f14ab0fc9835b4fb850aa968b
Max Wilson <max.wilson@nottingham.ac.uk>
2022-01-14 20:44:41 +0000

    im happy with part 2 of day 10 2020

--- 2020/10/10.py
+++ 2020/10/10.py
@@ -1,6 +1,7 @@
 from collections import Counter
 jolters = []
 distances = Counter()
+combinations = Counter()
 
 jolters.append(0)
 while True:
@@ -12,7 +13,33 @@ jolters = sorted(jolters)
 jolters.append(jolters[len(jolters)-1]+3)
 #print(jolters)
 
+def process_combos(x):
+    global combinations,jolters
+    options = 0
+    maxx = len(jolters)-1
+    y = 1
+    while True:
+        if x+y <= maxx:
+            if jolters[x+y] - jolters[x] <= 3:
+                options = options + combinations[x+y]
+            else:
+                break
+        else:
+            break
+        y+=1
+    return options
+
+
 for x in range(len(jolters)-1):
     distances[jolters[x+1]-jolters[x]]+=1
 
+#part 1
 print(distances[1]*distances[3])
+
+#part 2
+x = len(jolters)-2
+combinations[len(jolters)-1] = 1
+while x >= 0:
+    combinations[x] = process_combos(x)
+    x-=1
+print(combinations[0])
