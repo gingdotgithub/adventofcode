c78295820d91062a66bcd62f3c85dad01b8637b1
Max L. Wilson <max.wilson@nottingham.ac.uk>
2022-11-30 21:01:38 +0000

    testing rust on 2019, and setup 2022

--- 2019/aoc2019/src/1.rs
+++ 2019/aoc2019/src/1.rs
@@ -1,3 +1,20 @@
+
+
 fn main() {
-    println!("Hello, world!");
+    let my_str = include_str!("1.in");
+    let mut sumfuel = 0.0;
+    let data = my_str.split("\n");
+    for entry in data {
+        let mut entry: f32 = entry.parse().unwrap();
+        entry = ((entry / 3.0).floor()) -2.0;
+        sumfuel+= entry;
+    }
+    println!("{}",sumfuel);
 }
+
+
+// fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+// where P: AsRef<Path>, {
+//     let file = File::open(filename)?;
+//     Ok(io::BufReader::new(file).lines())
+// }
