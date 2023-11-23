6fdd19dec5aad3e765b1c1446d3939805673018f
Max L. Wilson <max.wilson@nottingham.ac.uk>
2023-11-23 12:18:05 +0000

    sorted 2019 day 3 part 2

--- 2019/3/3.py
+++ 2019/3/3.py
@@ -3,10 +3,11 @@ from numpy import *
 def processWire(w):
     x = 0
     y = 0
-    neww = []
-    #neww.append((x,y))
+    counter = 0
+    neww = {}
+    #neww.append((x,y)) - dont add the root, it becomes an intersection point
     for part in w:
-        print(part[1])
+        #print(part[1])
         for n in range(0,int(part[1:])):
             if part[0] == "R":
                 x = x+1
@@ -16,8 +17,26 @@ def processWire(w):
                 x = x-1
             else:
                 y = y+1
-            neww.append((x,y))
+            counter = counter + 1
+            if (x,y) not in neww.keys():
+                neww[(x,y)] = counter
     return neww.copy()
+
+
+def part1():
+    mandists = []
+    #print(intersections)
+    for i in intersections:
+        mandists.append(abs(i[0]) + abs(i[1]))
+    mandists.sort()
+    print("part 1: ", mandists[0])
+
+def part2():
+   stepdists = []
+   for i in intersections:
+       stepdists.append(wire1map[i] + wire2map[i])
+   stepdists.sort()
+   print("part 2: ", stepdists[0])
 
 
 wire1 = []
@@ -26,13 +45,8 @@ with open('3.in') as f:
     wire1 = f.readline().split(",")
     wire2 = f.readline().split(",")
 
-wire1 = processWire(wire1)
-wire2 = processWire(wire2)
-
-intersections = list(set(wire1) & set(wire2))
-mandists = []
-print(intersections)
-for i in intersections:
-    mandists.append(abs(i[0]) + abs(i[1]))
-mandists.sort()
-print(mandists)
+wire1map = processWire(wire1)
+wire2map = processWire(wire2)
+intersections = list(set(wire1map.keys()) & set(wire2map.keys()))
+part1()
+part2()
