ebd6690dc54fee2151d2c8caa9497f65cd5d3b4b
Max L. Wilson <max.wilson@nottingham.ac.uk>
2023-11-26 23:53:53 +0100

    sorted 2019 day 10 part 2 finally

--- 2019/10/10.py
+++ 2019/10/10.py
@@ -13,18 +13,25 @@ def calculate(datatouse):
 
     bestsite = (-1,-1)
     bestsitecount = -1
+    bestsetangles = {} #a dict that will be a set of all angles from the best site's point of view
     for source in asteroids.keys():
-        angles = []
+        angles = {} #converted to a dict for part 2, to hold a list for every angle
         for asteroid in asteroids.keys():
             if asteroid != source:
                 angle = math.atan2(asteroid[0]-source[0],asteroid[1]-source[1])
+                distance = math.sqrt((asteroid[0]-source[0])**2) + ((asteroid[1]-source[1])**2) #calc distance for part 2, as we go
                 #print(angle)
-                angles.append(angle)
-        if len(set(angles)) > bestsitecount:
-            bestsitecount = len(set(angles))
+                if angle not in angles.keys():
+                    angles[angle] = {} #also a dict, holding a coordinate for evey distance, so we can sort by distance later
+                angles[angle][distance] = asteroid
+        if len(angles.keys()) > bestsitecount:
+            bestsitecount = len(angles.keys())
             bestsite = source
+            #print(angles)
+            bestsetangles = angles.copy()
         #print(len(set(angles)))
     print("best site: ", bestsite, " and its count ", bestsitecount) 
+    return dict(sorted(bestsetangles.items(),key=lambda x:x[0],reverse=True)) #reverse because north is the biggest angle from atan2
         # for asteroid in asteroids.keys():
         #     x = 0
 
@@ -36,8 +43,26 @@ def test():
     calculate('10-5.test')
 
 def part1():
-    calculate('10.in')
+    return calculate('10.in')
+
+def part2(bestsetangles):
+    #print(bestsetangles)
+    counter = 0
+    for angle in bestsetangles.keys():
+        bestsetangles[angle] = dict(sorted(bestsetangles[angle].items()))
+        if len(bestsetangles[angle].keys()) > 0:
+            counter = counter + 1
+            angledists = list(bestsetangles[angle].keys())
+            #print(angledists[0])
+            if counter == 200:
+                print((bestsetangles[angle][angledists[0]][0]*100)+bestsetangles[angle][angledists[0]][1])
+
+            del bestsetangles[angle][angledists[0]]
+        else:
+            del bestsetangles[angle]
+        
+
 
 test()
-part1()
-
+bestsetangles = part1()
+part2(bestsetangles.copy())
