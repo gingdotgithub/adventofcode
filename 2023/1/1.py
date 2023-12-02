181f0934fad3123f2c977ea87bb25505b58ac4bc
Max L. Wilson <max.wilson@nottingham.ac.uk>
2023-12-02 09:50:42 +0000

    updated day 1 to be more organised, just for interests sake

--- 2023/1/1.py
+++ 2023/1/1.py
@@ -4,7 +4,6 @@ def process(filename, addfilter):
     with open(filename) as f:
         dataset = f.readlines()
 
-    output = 0
     for line in dataset:
 
         if addfilter:
@@ -33,28 +32,69 @@ def findReplacements(line, rightToLeft):
         temp = 1
         for x in range(len(line)-1,0,-1):
             if line[x].isnumeric():
-                print("sub not needed")
+                #print("sub not needed")
                 return line
             for c in conv.keys():
                 if (x+len(c)) < len(line):
                     if line[x:x+len(c)] == c:
                         newline = line[0:x] + conv[c] + line[x+len(c):]
-                        print("RtL replaced",c,"with",conv[c],".",line," is now ", newline)
+                        #print("RtL replaced",c,"with",conv[c],".",line," is now ", newline)
                         return newline
     else: 
         for x in range(0,len(line)-2):
             if line[x].isnumeric():
-                print("sub not needed")
+                #print("sub not needed")
                 return line
             for c in conv.keys():
                 if (x+len(c)) < len(line):
                     if line[x:x+len(c)] == c:
                         newline = line[0:x] + conv[c] + line[x+len(c):]
-                        print("Replaced",c,"with",conv[c],".",line," is now ", newline)
+                        #print("Replaced",c,"with",conv[c],".",line," is now ", newline)
                         return newline
-    print("none found", line)
+    #print("none found", line)
     return line
 
+
+def propperPart2(filename):
+    conv = {
+        "1":"1",
+        "2":"2",
+        "3":"3",
+        "4":"4",
+        "5":"5",
+        "6":"6",
+        "7":"7",
+        "8":"8",
+        "9":"9",
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9"
+    }
+    with open(filename) as f:
+        dataset = f.readlines()
+
+    output = 0
+    sumofparts = 0
+    for line in dataset:
+        inputrange = range(0,len(line))
+        leftpart = processPart2(line,inputrange,conv)
+        inputrange = reversed(inputrange)
+        rightpart = processPart2(line,inputrange,conv)
+        sumofparts = sumofparts + int(leftpart+rightpart)
+    print("part 2 done properly:",sumofparts)
+
+def processPart2(line, inputrange, conv):
+    for x in inputrange:
+        for c in conv.keys():
+            if line[x:x+len(c)] == c:
+                return conv[c]
+                    
 
 def test():
     print("test answer is:",process('1.test', False))
@@ -69,6 +109,9 @@ def part2():
     print("part 2:", process('1.in', True))
 
 test()
-test2()
 part1()
+test2()
 part2()
+
+propperPart2('1-2.test')
+propperPart2('1.in')
