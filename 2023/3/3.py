5db710dbbe62b2e95b327e7506fce46be6149c56
Max L. Wilson <max.wilson@nottingham.ac.uk>
2023-12-03 21:12:09 +0000

    sorted day 2 part 2

--- 2023/3/3.py
+++ 2023/3/3.py
@@ -1,5 +1,6 @@
 numbers = {}
 symbols = {}
+symbollinks = {}
 
 def check(xrange,yrange):
     for x in xrange:
@@ -8,9 +9,10 @@ def check(xrange,yrange):
                 print("found symbol",symbols[(x,y)])
                 return True
     return False
+                    
 
-def processGrid():
-    with open('3.in') as f:
+def processGrid(filename):
+    with open(filename) as f:
         engine = f.readlines()
 
     currnumber = ""
@@ -47,7 +49,28 @@ def part1():
         if check(xrange,yrange) == True:
             print("found symbol, adding",numbers[(gridrefx,gridrefy)])
             answer += int(numbers[(gridrefx,gridrefy)])
-    print(answer)
+    print("part 1:",answer)
 
-processGrid()
+def part2():
+    answer = 0
+    for gridrefx,gridrefy in numbers:
+        xrange = range(gridrefx-1,gridrefx+len(numbers[(gridrefx,gridrefy)])+1)
+        yrange = range(gridrefy-1,gridrefy+2)
+        for x in xrange:
+            for y in yrange:
+                if (x,y) in symbols.keys():
+                    if symbols[(x,y)] == "*":
+                        if (x,y) not in symbollinks.keys():
+                            symbollinks[(x,y)] = []
+                        symbollinks[(x,y)].append((gridrefx,gridrefy))
+    #print(symbollinks)
+    for sx,sy in symbollinks.keys():
+        gears = symbollinks[(sx,sy)]
+        if len(gears) == 2:
+            answer+=(int(numbers[gears[0]]) * int(numbers[gears[1]]))
+    print("part 2:",answer)
+
+processGrid("3.in")
+#processGrid("3.test")
 part1()
+part2()
