d0897d10f42e6bd612356d903b8c8728f63aef81
Max L. Wilson <max.wilson@nottingham.ac.uk>
2023-12-04 09:43:02 +0000

    2023 day 4 part 2

--- 2023/4/4.py
+++ 2023/4/4.py
@@ -1,3 +1,6 @@
+wincounts = {}
+cardcounts = {}
+
 def part1(filename):
     with open(filename) as f:
         cards = f.readlines()
@@ -9,11 +12,22 @@ def part1(filename):
         wnums = winning.split()
         rnums = received.split()
         overlapping = set(wnums) & set(rnums)
-        print(cardid,overlapping)
+        #print(cardid,overlapping)
+        cardnum = cardid.split()
+        wincounts[cardnum[1]] = len(overlapping) #for part 2
+        cardcounts[cardnum[1]] = 1 #for part 2
         if len(overlapping) > 0:
             answer+= 2**(len(overlapping)-1)
 
-    print(answer)
+    print("Part 1:",answer)
+
+def part2():
+    for x in range(1,len(wincounts)+1):
+        for n in range(0,wincounts[str(x)]):
+            cardcounts[str(x+1+n)] += cardcounts[str(x)]
+        #print("doing card ",x,"-",cardcounts)
+    print("Part 2:",sum(cardcounts.values()))
 
 part1('4.in')
 #part1('4.test')
+part2()
