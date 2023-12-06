d36a3a3aec0dc0d232e1060469a68ec3443ad61f
Max L. Wilson <max.wilson@nottingham.ac.uk>
2023-12-06 17:48:43 +0000

    2023 day 6 part 2 - strangely easy

--- 2023/6/6.py
+++ 2023/6/6.py
@@ -7,8 +7,12 @@ def process(filename):
     with open(filename) as f:
         data = f.readlines()
 
-    times = list(map(int,data[0].strip().split()[1:]))
-    distances = list(map(int,data[1].strip().split()[1:]))
+    #part 1
+    # times = list(map(int,data[0].strip().split()[1:]))
+    # distances = list(map(int,data[1].strip().split()[1:]))
+    #part 2
+    times.append(int(''.join(data[0].strip().split()[1:])))
+    distances.append(int(''.join(data[1].strip().split()[1:])))
 
     print(times)
     print(distances)
@@ -16,18 +20,18 @@ def process(filename):
 
 def part1():
     answers = []
-    print(times)
     for x in range(0,len(times)):
         count = 0
         for n in range(1,times[x]):
             distance = ((times[x]-n)*n)
-            print(distance)
+            #print(distance)
             if distance > distances[x]:
                 count += 1
         answers.append(count)
-    print(math.prod(answers))
+    print("answer:",math.prod(answers))
 
 
 times,distances = process('6.in')
-part1()
+part1() 
+#part 2 see changes in processing
 
