8680e08f037f4d56660a85f55fb551a884e303ae
Max L. Wilson <max.wilson@nottingham.ac.uk>
2023-12-15 10:14:20 +0000

    2023 day 15 part 2 ok - thank goodness python dicts hold their ordering, that saves some effort.

--- 2023/15/15.py
+++ 2023/15/15.py
@@ -1,14 +1,52 @@
+import re
+
 with open('15.in') as f:
     data = f.readline()
 
 data = data.split(",")
-answer = 0
-for x in data:
+def part1():
+    answer = 0
+    for x in data:
+        answer+=hashit(x)
+    print("part 1:",answer)
+
+def hashit(value):
     runningval = 0
-    for n in range(0,len(x)):
-        runningval+=ord(x[n])
+    for n in range(0,len(value)):
+        runningval+=ord(value[n])
         runningval*=17
         runningval = runningval%256
-        print(runningval)
-    answer+=runningval
-print("part 1:",answer)
+        #print(runningval)
+    return runningval
+
+def part2():
+    hashmap = {}
+    for x in data:
+        if "=" in x:
+            hashbit,val = x.split("=")
+            symbol = "="
+        else:
+            hashbit = x[:len(x)-1]
+            symbol = "-"
+            val=""
+        #print(hashbit,symbol,val)
+        boxid = hashit(hashbit)
+        if boxid not in hashmap.keys():
+            hashmap[boxid] = {}
+        if symbol in "=":
+            #if hashbit not in hashmap[boxid].keys()
+            hashmap[boxid][hashbit] = val
+        if symbol in "-":
+            if hashbit in hashmap[boxid].keys():
+                hashmap[boxid].pop(hashbit)
+        #print(hashmap)
+    answer = 0
+    for box in hashmap.keys():
+        lenscount = 0
+        for lens in hashmap[box].keys():
+            lenscount+=1
+            answer += ((box+1) * lenscount * int(hashmap[box][lens]))
+    print("Part 2:",answer)
+
+part1()
+part2()
