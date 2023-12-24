675429a0579f63b3a45cfb8af0ad5358e1700aef
Max L. Wilson <max.wilson@nottingham.ac.uk>
2023-12-24 11:28:30 +0000

    2023 day 23 part 2 - solved in 4.5hrs lol. once i used a single visit queue rather than copying queues into each recusrive call.

--- 2023/23/23.py
+++ 2023/23/23.py
@@ -2,14 +2,15 @@ import time
 import sys
 
 sys.setrecursionlimit(10000)
-data = open('23.test').read().splitlines()
+data = open('23.in').read().splitlines()
 starttime = time.time()
 
 sx,sy = (1,0)
 ex,ey = (len(data[0])-2,len(data)-1)
+visited = set()
 
-def findpath(sx,sy,route):
-    route.add((sx,sy))
+def findpath(sx,sy,depth):
+    #route.add((sx,sy))
     steps = [(sx+1,sy),(sx,sy+1),(sx-1,sy),(sx,sy-1)]
     notallowed = ["<","^",">","v"]
     returnval = 0
@@ -17,17 +18,19 @@ def findpath(sx,sy,route):
         nextstep = steps[possstep]
         nextval = data[nextstep[1]][nextstep[0]]
         if nextstep == (ex,ey):
-            print("found an end",len(route),". max is now",returnval)
+            print("found an end",depth+1)
             #f = input("?")
-            return len(route)
-        if nextstep not in route and nextval != "#" and nextval != notallowed[possstep]: # in [".","v","^","<",">"]:
+            return depth+1
+        if nextstep not in visited and nextval != "#": # and nextval != notallowed[possstep]: # in [".","v","^","<",">"]:
             #print("going",nextstep, "which is",nextval)
-            answer = findpath(nextstep[0],nextstep[1],route.copy())
+            visited.add(nextstep)
+            answer = findpath(nextstep[0],nextstep[1],depth+1)
+            visited.remove(nextstep)
             if answer > returnval:
                 returnval = answer
     return returnval
     
 # print((ex,ey))
 # print(len(data[0]))
-print("part 1:",findpath(sx,sy,set()))
+print("part 1:",findpath(sx,sy,0))
 print("part 1 timing:",time.time()-starttime)
