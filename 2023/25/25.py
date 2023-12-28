88df9b3e446c4fb79329d832016f8843f664ac1b
Max L. Wilson <max.wilson@nottingham.ac.uk>
2023-12-28 08:39:25 +0000

    2023 day 25 - bit of a cheat easy once you know about networkx and its functions.

---
+++ 2023/25/25.py
@@ -0,0 +1,25 @@
+import networkx
+import time
+
+data = open('25.in').read().splitlines()
+starttime = time.time()
+g = networkx.Graph()
+
+for line in data:
+    node,neighbours = line.split(": ")
+    neighbours = neighbours.split(" ")
+    #print(neighbours)
+    for n in neighbours:
+        #print('adding',node,n)
+        g.add_edge(node,n)
+        #g.add_edge(n,node)
+
+print(g)
+g.remove_edges_from(networkx.minimum_edge_cut(g))
+print(g)
+a,b = networkx.connected_components(g)
+print(a,b)
+endtime = time.time()
+print("part 1:",len(a)*len(b))
+print("part 1 timing:",endtime-starttime)
+#once you learn about networkx and graphs, then this one is a bit of a cheat-easy
