277c26254d42a823c95a2663b916ff7567099a3d
Max L. Wilson <max.wilson@nottingham.ac.uk>
2024-11-27 14:46:07 +0000

    something changed in day 1... who knows

--- 2023/day1/src/main.rs
+++ 2023/day1/src/main.rs
@@ -1,7 +1,9 @@
 use regex::Regex;
+use std::collections::HashMap;
 
 fn main() {
-    part1()
+    part1();
+    part2();
 }
 
 fn part1() {
@@ -15,6 +17,31 @@ fn part1() {
         let combined = format!("{}{}",&result[0..1],&result[strlen-1..]);
         answer += combined.parse::<u32>().unwrap();
     }
+    println!("{}",answer);
+}
+
+fn part2() {
+    let mut answer:u32 = 0;
+    let conversions = HashMap::from([
+        ("1","1"),
+        ("2","2"),
+        ("3","3"),
+        ("4","4"),
+        ("5","5"),
+        ("6","6"),
+        ("7","7"),
+        ("8","8"),
+        ("9","9"),
+        ("one", "1"),
+        ("two", "2"),
+        ("three", "3"),
+        ("four", "4"),
+        ("five", "5"),
+        ("six", "6"),
+        ("seven", "7"),
+        ("eight", "8"),
+        ("nine", "9"),
+    ]);
     println!("{}",answer);
 }
 
