c70a720f99af8030ff4e6719d71e19c2511f35b8
Max L. Wilson <max.wilson@nottingham.ac.uk>
2024-12-01 08:15:06 +0000

    2024 day 1 complete. rushed low-MWL pre-tidy code

--- 2024/1/1.py
+++ 2024/1/1.py
@@ -15,9 +15,25 @@ print(set1)
 print(set2)
 print()
 
+#part 1
 gap = 0
 for x in range(len(set1)):
     gap = gap + abs(set1[x]-set2[x])
 
 print(gap)
 
+#part 2
+simlist = {}
+for x in range(len(set2)):
+    if set2[x] in set1:
+        if set2[x] not in simlist.keys():
+            simlist[set2[x]] = 0
+        simlist[set2[x]] += set2[x]
+
+print(simlist)
+gap = 0
+for x in range(len(set1)):
+    if set1[x] in simlist.keys():
+        gap += simlist[set1[x]]
+        print("added!")
+print(gap)
