bade1fe18802ac8415952639e9c0274b4988a6f1
Max L. Wilson <max.wilson@nottingham.ac.uk>
2024-12-05 11:22:00 +0000

    2024 day 5 sorted

--- 2024/5/5.py
+++ 2024/5/5.py
@@ -1,7 +1,8 @@
-with open('5.test') as f:
+with open('5.in') as f:
     dataset = f.readlines()
     dataset = [line.rstrip('\n') for line in dataset]
 
+# Reading in rules
 x = 0
 mustBeBefore = {}
 mustBeAfter = {}
@@ -22,29 +23,49 @@ print(mustBeBefore)
 
 x+=1
 
-
+# doing Part 1 and Part 2
 totalOfMiddles = 0
+totalOfBadMiddles = 0
 while x < len(dataset):
     update = dataset[x].split(",")
     print(update)
     z = len(update)
     badUpdate = False
-    for a in range(0,z):
-        for b in range(0,z):
+    toSwap = False
+    a = 0
+    while a < z:
+        b = 0
+        while b < z:
             if a < b:
                 if update[a] not in mustBeAfter.keys() or update[b] not in mustBeAfter[update[a]]:
                     print(update[b], "not in mustBeAfter",update[a])
                     badUpdate = True
+                    toSwap = True
             if a > b:
                 if update[a] not in mustBeBefore.keys() or update[b] not in mustBeBefore[update[a]]:
                     print(update[b],"not in mustBeBefore",update[a])
                     badUpdate = True
+                    toSwap = True
+            if toSwap: #for badupdates, toswap swaps them and starts the validation again
+                print("swapping....")
+                print(update)
+                update[a], update[b] = update[b], update[a]
+                a = 0
+                b = 0
+                print(update)
+                toSwap = False
+            b+=1
+        a+=1
 
     if not badUpdate:
         totalOfMiddles+= int(update[int(z/2)])
+    else:
+        totalOfBadMiddles+= int(update[int(z/2)])
 
     x+=1
 
+# printing out the answer
 print("part1",totalOfMiddles)
+print("part2", totalOfBadMiddles)
 
 
