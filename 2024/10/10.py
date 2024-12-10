1de01f965b32f11d88e916e73998663df8eb938c
Max L. Wilson <max.wilson@nottingham.ac.uk>
2024-12-10 09:37:12 +0000

    2024 day 10 complete

--- 2024/10/10.py
+++ 2024/10/10.py
@@ -1,27 +1,26 @@
+import time
+
+time1=time.time()
 with open('10.in') as f:
     dataset = f.readlines()
     dataset = [line.rstrip('\n') for line in dataset]
-
+time2=time.time()
 
 zeros = []
 h = len(dataset)
 w = len(dataset[0])
-print(w,h)
+# print(w,h)
 
 
-def dfs(visited,x,y):
-    if (x,y) not in visited:
-        visited.add((x,y))
-        for neighbour in getneighbours(x,y):
-            if dataset[neighbour[1]][neighbour[0]] != '.':
-                if int(dataset[neighbour[1]][neighbour[0]]) - int(dataset[y][x]) == 1:
-                    if dataset[neighbour[1]][neighbour[0]] == '9':
-                        print('found one',neighbour[0],neighbour[1])
-                        if [neighbour[0],neighbour[1]] not in trailends:
-                            print("and its new!")
-                            trailends.append([neighbour[0],neighbour[1]])
-                    else:
-                        dfs(visited,neighbour[0],neighbour[1])
+def dfs(x,y):
+    for neighbour in getneighbours(x,y):
+        if dataset[neighbour[1]][neighbour[0]] != '.':
+            if int(dataset[neighbour[1]][neighbour[0]]) - int(dataset[y][x]) == 1:
+                if dataset[neighbour[1]][neighbour[0]] == '9':
+                    if [neighbour[0],neighbour[1]] not in trailends:
+                        trailends.append([neighbour[0],neighbour[1]])
+                else:
+                    dfs(neighbour[0],neighbour[1])
 
 def getneighbours(x,y):
     neighbours = [[x,y-1],[x+1,y],[x,y+1],[x-1,y]]
@@ -31,18 +30,47 @@ def getneighbours(x,y):
             goodneighbours.append(coord)
     return goodneighbours
 
-visited = set()
 trailends = []
 score = 0
 for y in range(h):
     for x in range(w):
         if dataset[y][x] == '0':
             trailends = []
-            visited = set()
             zeros.append([x,y])
-            print("doing",x,y)
-            dfs(visited,x,y)
+            # print("doing",x,y)
+            dfs(x,y)
             score += len(trailends)
-print(zeros)
+# print(zeros)
 print("part 1",score)
+time3=time.time()
 
+def dfspart2(x,y):
+    for neighbour in getneighbours(x,y):
+        if dataset[neighbour[1]][neighbour[0]] != '.':
+            if int(dataset[neighbour[1]][neighbour[0]]) - int(dataset[y][x]) == 1:
+                if dataset[neighbour[1]][neighbour[0]] == '9':
+                    # if [neighbour[0],neighbour[1]] not in trailends:
+                    trailends.append([neighbour[0],neighbour[1]])
+                else:
+                    dfspart2(neighbour[0],neighbour[1])
+
+# print("###############")
+# print("starting part 2")
+
+trailends = []
+score = 0
+for y in range(h):
+    for x in range(w):
+        if dataset[y][x] == '0':
+            trailends = []
+            zeros.append([x,y])
+            # print("doing",x,y)
+            dfspart2(x,y)
+            score += len(trailends)
+print("part 2:",score)
+
+time4=time.time()
+
+print("read in time:",time2-time1)
+print("part 1 time :", time3-time2)
+print("part 2 time :", time4-time3)
