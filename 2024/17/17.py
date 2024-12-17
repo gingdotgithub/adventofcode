55c77f1322ffa67b52a35f7b819133dcbaccc38b
Max L. Wilson <max.wilson@nottingham.ac.uk>
2024-12-17 14:52:06 +0000

    2024 day 17 part 1

---
+++ 2024/17/17.py
@@ -0,0 +1,87 @@
+import time
+
+
+time1=time.time()
+with open('17.in') as f:
+    dataset = f.readlines()
+    dataset = [line.rstrip('\n') for line in dataset]
+    dataset = [line.split(": ") for line in dataset]
+
+registerIDs = "ABC"
+registers = []
+ops = list(map(int,dataset[4][1].split(",")))
+registers.append(int(dataset[0][1]))
+registers.append(int(dataset[1][1]))
+registers.append(int(dataset[2][1]))
+ip = 0
+mainoutput = []
+
+print(registers)
+print(ops)
+time2 = time.time()
+def getcombooperand(operand):
+    if 3 < operand < 7:
+        operand = registers[operand-4]
+    return operand
+
+def process(opcode, operand,ip):
+    if opcode == 0:
+        operand = getcombooperand(operand)
+        registers[0] = int(registers[0] / (2**operand))
+        return ip+2
+    elif opcode == 1:
+        registers[1] = registers[1] ^ operand
+        return ip+2
+    elif opcode == 2:
+        registers[1] = getcombooperand(operand) % 8
+        return ip+2
+    elif opcode == 3:
+        if registers[0] != 0:
+            return operand
+        return ip+2
+    elif opcode == 4:
+        registers[1] = registers[1] ^ registers [2]
+        return ip+2
+    elif opcode == 5:
+        mainoutput.append(str(getcombooperand(operand) % 8))
+        return ip+2
+    elif opcode == 6:
+        operand = getcombooperand(operand)
+        registers[1] = int(registers[0] / (2**operand))
+        return ip+2
+    elif opcode == 7:
+        operand = getcombooperand(operand)
+        registers[2] = int(registers[0] / (2**operand))
+        return ip+2
+
+def part1(ip):
+    while ip < len(ops):
+        opcode = ops[ip]
+        operand = ops[ip+1]
+        # print("doing",opcode,operand,ip)
+        ip = process(opcode,operand,ip)
+        # print(registers)
+        # test = input()
+
+part1(ip)
+time3 = time.time()
+print("Part 1:",",".join(mainoutput))
+print("Part 1 time:",time3-time2)
+
+
+val = 10000000
+mainoutput = []
+opscopy = [str(x) for x in ops]
+while mainoutput != opscopy:
+    val+=1
+    # print("trying",val)
+    # print(mainoutput,ops)
+    mainoutput = []
+    registers[0] = val
+    registers[1] = 0
+    registers[2] = 0
+    ip = 0
+    part1(ip)
+    if ",".join(mainoutput).endswith("5,4,1,5,5,0,3,3,0"):
+        print(val,"made",",".join(mainoutput))
+    # test = input()
