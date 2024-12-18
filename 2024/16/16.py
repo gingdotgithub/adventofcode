57bfe07e396ca2810e733f370cc1ab859189f02a
Max L. Wilson <max.wilson@nottingham.ac.uk>
2024-12-18 08:45:09 +0000

    finally fixed 2024 day 16 part 2

--- 2024/16/16.py
+++ 2024/16/16.py
@@ -1,6 +1,8 @@
 import time
-import math
 import heapq
+import sys
+
+sys.setrecursionlimit(100000)
 
 time1=time.time()
 with open('16.in') as f:
@@ -11,6 +13,7 @@ grid = []
 ns = []
 ne = []
 facing = (1,0)
+visited = {}
 dirs = {
     (1,0)  : [[0,-1,1001],[1,0,1],[0,1,1001]],
     (0,1)  : [[1,0,1001],[0,1,1],[-1,0,1001]],
@@ -26,14 +29,8 @@ for y in range(len(dataset)):
         ns = [dataset[y].index('S'),y]
     grid.append(list(dataset[y]))
 
-print("start",ns,"end",ne)
-print(grid)
-# def hval(src):
-#     a = src[0]
-#     b = src[1]
-#     x = ne[0]
-#     y = ne[1]
-#     return ((x-a)**2 + (y-b)**2)**0.5
+# print("start",ns,"end",ne)
+# print(grid)
 
 def printgrid():
     for row in grid:
@@ -41,12 +38,13 @@ def printgrid():
 
 def part1(facing):
     openlist = []
-    visited = []
+    global visited
     heapq.heappush(openlist,(0.0,ns[0],ns[1],facing))
 
     while len(openlist) > 0:
         nn = heapq.heappop(openlist)
-        visited.append([nn[1],nn[2]])
+        if (nn[1],nn[2]) not in visited.keys() or ((nn[1],nn[2]) in visited.keys() and nn[0] < visited[(nn[1],nn[2])]):
+            visited[(nn[1],nn[2])] = nn[0]
         facing = nn[3]
         # printgrid()
         # print("checking",nn)
@@ -57,7 +55,59 @@ def part1(facing):
             newy = nn[2]+dir[1]
             if [newx,newy] == ne:
                 return nn[0]+dir[2]
-            if [newx,newy] not in visited and grid[newy][newx] == '.':
+            print(nn[0])
+            if (newx,newy) not in visited.keys() and grid[newy][newx] == '.':
                 heapq.heappush(openlist,(nn[0]+dir[2],newx,newy,(dir[0],dir[1])))
-    
-print("part 1:",part1(facing))
+
+time2 = time.time()    
+bestdistance = part1(facing)
+print("visited:",visited, len(visited))
+uniquevis = set(visited)
+print("unique",len(uniquevis))
+print("part 1:",bestdistance)
+test = input()
+goodseats = set()
+dfsvisited = []
+def part2(nn,facing,cost):
+    global visited
+    global dfsvisited
+    global goodseats
+    global bestdistance
+    if nn not in dfsvisited:
+        dfsvisited.append(nn)
+        # print("doing",nn,dfsvisited)
+        if grid[nn[1]][nn[0]] == 'E':
+            dfsvisited.remove(nn)
+            print("cost",cost,"best",bestdistance)
+            # test = input()
+            if cost == bestdistance:
+                # print("cost",cost,"best",bestdistance)
+                return True
+            else:
+                return False
+        if cost > bestdistance:
+            print("too long now",cost)
+            dfsvisited.remove(nn)
+            return False
+        if cost > visited[(nn[0],nn[1])]+1000:
+            print("gone a suboptimal route to here")
+            dfsvisited.remove(nn)
+            return False
+        for dir in dirs[facing]:
+            newx = nn[0]+dir[0]
+            newy = nn[1]+dir[1]
+            # if (newx,newy) in visited.keys() and (grid[newy][newx] == '.' or grid[newy][newx] == 'E'): #and (newx,newy) in visited
+            if (newx,newy) in visited.keys() or grid[newy][newx] == 'E':
+                result = part2((newx,newy),(dir[0],dir[1]),cost+dir[2])
+                if result:
+                    goodseats.update(dfsvisited)
+                    
+        dfsvisited.remove(nn)
+    # return result
+
+part2((ns[0],ns[1]),facing,0)
+time3 = time.time()
+# print(goodseats,len(goodseats))       
+print("Part 2",len(goodseats)+1)
+print("part 1 time:",time2-time1)
+print("Part 2 time:",time3-time2)
